4ad251c8121b0c62c210aa92c2987b9d08bb5b44
yufao <1316842739@qq.com>
2025-05-17 17:51:24 +0800

    init call

---
+++ Call.java
@@ -0,0 +1,13 @@
+class Call {
+
+    public static void main(String[] args) {
+        // Create an instance of the Call class
+        Call call = new Call();
+        // Call the method to print the message
+        call.printMessage();
+    }
+
+    public void printMessage() {
+        System.out.println("Hello, Call!");
+    }
+}
